57f9b4dc8398603c58734f3a1ea20fb2442d0322
rjasl <junwe99@gmail.com>
2020-07-26 09:53:05 +0900

    모음 긴거 추가

--- hanbon.js
+++ hanbon.js
@@ -28,13 +28,39 @@ function getJapnaeseToKorean(letter)
     // 행은 자음
     // 단은 모음이다.
 
-    const fistIndex = compareFistLetter[letter.f];
-    const secIndex= compareSecLetter[letter.s];
-    console.log("fistIndex : " + fistIndex);
-    console.log("secIndex : " + secIndex);
-    console.log(JP_NOMAL_letter[fistIndex][secIndex]);
+    const firstLetter = getSplitSecondLltter(letter);
+    const thridIndex = compareThirdLetter[letter.t]
 
-    return JP_NOMAL_letter[fistIndex][secIndex];
+    let thridLetter = JP_SYLLABLE_LETTER[thridIndex];
+
+    if(thridLetter == null)
+        thridLetter = "";
+
+    console.log(firstLetter+thridLetter);
+
+    return  firstLetter+thridLetter;
+}
+
+function getSplitSecondLltter(letter)
+{
+    let firstIndex = compareFistLetter[letter.f];
+    let secIndex = compareSecLetter[letter.s];
+    //긴 글자이다
+    if(compareSecLetter[letter.s] == null)
+    {
+        secIndex = comapreSecLetter_long[letter.s];
+        return JP_LONG_LTEER[firstIndex][secIndex];
+    }
+    //모음 분리 필요
+    else if(secIndex >= 10)
+    {
+        return JP_NOMAL_letter[firstIndex][secIndex];
+    }
+    // 평범한 글자
+    else
+    {
+        return JP_NOMAL_letter[firstIndex][secIndex];
+    }
 }
 
 // 한글 분리하는 함수
@@ -73,7 +99,7 @@ init();
 
 
 
-
+// 한글 분리를 위해 사용
 const KOREAN_FIRST_LETTER = 
 ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ',
 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ',
@@ -88,9 +114,10 @@ const KOREAN_LAST_LETTER =
 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ',
 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
 
+// 히라가나 기본 50음도
 const JP_NOMAL_letter = 
 [
-//   0    1    2    3   4
+//    0    1    2    3   4
     ['あ','い','う','え','お'],//0
     ['か','き','く','け','こ'],//1
     ['さ','し','す','せ','そ'],//2
@@ -109,25 +136,25 @@ const JP_NOMAL_letter =
 const compareFistLetter = 
 {
     'ㄱ' : 8,'ㄲ' : 1,'ㄴ' : 4,'ㄷ' : 10,'ㄸ' : 10,'ㄹ' : 7,'ㅁ' : 6,'ㅂ' : 11,
-    'ㅃ' : 12,'ㅅ' : 2,'ㅆ' : 2,'ㅇ' : 0,'ㅈ' : 9,'ㅉ' : 9,'ㅊ' : 3,'ㅋ' : 1,'ㅌ' : 3,'ㅍ' : 11,'ㅎ' : 5
+    'ㅃ' : 12,'ㅅ' : 2,'ㅆ' : 2,'ㅇ' : 0,'ㅈ' : 9,'ㅉ' : 9,'ㅊ' : 3,'ㅋ' : 1,'ㅌ' : 3,'ㅍ' : 12,'ㅎ' : 5
 };
-
+// 중성
 const compareSecLetter = 
 {
     'ㅏ':0,'ㅐ':3,'ㅒ':3,'ㅣ':1,'ㅓ':4,'ㅔ':3,
     'ㅗ':4,'ㅜ':2,'ㅡ':2,
-    'ㅙ':40,'ㅚ':01,'ㅝ':43,'ㅞ':43,
-    'ㅟ':41,'ㅢ':41
+    'ㅙ':43,'ㅚ':41,'ㅝ':24,'ㅞ':23,
+    'ㅟ':21,'ㅢ':21
 }
-
+// 특별 중성
 const comapreSecLetter_long =
 {
-    'ㅑ' : 0,'ㅠ':1,'ㅛ':2,'ㅘ':3,
+    'ㅑ' : 0,'ㅠ':1,'ㅛ':2,'ㅘ':3,'ㅕ':2,'ㅖ':2
 }
 /*
-[,
-'ㅖ',, 'ㅙ', 'ㅚ', 'ㅛ', ,
-'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', , 'ㅢ', ];
+['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ',
+'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ',
+'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'];
 */
 
 const JP_LONG_LTEER = 
@@ -146,13 +173,17 @@ const JP_LONG_LTEER =
     ['びゃ','びゅ','びょ'],
     ['ぴゃ','ぴゅ','ぴょ'],  
 ];
-
+// 받침
+const compareThirdLetter = 
+{
+    'ㄱ':3, 'ㄲ':3, 'ㄳ':15, 'ㄴ':1, 'ㄵ':15, 'ㄶ':15,
+    'ㄷ':0, 'ㄹ':9, 'ㄺ':15, 'ㄻ':15, 'ㄼ':15, 'ㄽ':15, 'ㄾ':15,
+    'ㄿ':15, 'ㅀ':15, 'ㅁ':8, 'ㅂ':13, 'ㅄ':15 ,'ㅅ':0, 'ㅆ':0,
+    'ㅇ':1, 'ㅈ':0, 'ㅊ':0, 'ㅋ':3, 'ㅌ':0, 'ㅍ':14, 'ㅎ':15
+};
+// 받침 일본어
 const JP_SYLLABLE_LETTER = 
 [
-    'っ','ん'
+//  0     1    2    3    4   5    6    7    8    9   10   11   12   13   14  15
+    'っ','ん','う','く','す','つ','ぬ','ふ','む','る','ぐ','ず','づ','ぶ','ぷ',''
 ]
-// 중성, 단의 번호를 적는다.
-const compareMiddleLetter = 
-{
-
-}
